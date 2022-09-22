d8dc836dd5d4a08d27228eead5f6c6b2ae998cf2
ValeriaGomez12 <cinthi978@gmail.com>
2022-09-22 09:34:11 -0500

    cer

---
+++ modulo2/lab2.1.1.6.py
@@ -0,0 +1,8 @@
+'''
+Descripccion programa muy facil la funcion print 
+Autor: Valeria Gomez 
+fecha:20 de septiembre del 2022
+'''
+print("Hola Mundo")
+print("Soy paquito")
+print("Bienvenido a python")
