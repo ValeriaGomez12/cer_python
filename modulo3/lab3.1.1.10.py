9ac1581f6d21085d2fbcf2ae96965609bc66cd91
ValeriaGomez12 <cinthi978@gmail.com>
2022-09-27 10:06:07 -0500

    captura y laboratorios

---
+++ modulo3/lab3.1.1.10.py
@@ -0,0 +1,13 @@
+'''
+Descripccion:Familiarizarse con la función input().
+Autor: Valeria Gomez
+Fecha: 27 de sep del 2022
+'''
+
+
+planta = input('Ingresa el nombre de la planta')
+if planta=="ESPATIFILO":
+   print("si, ¡EL ESPATIFILO! es la mejor planta de todos los tiempos !")
+
+if planta =="espatifilo":
+   print("No, ¡quiero un gran ESPATIFILO!")
